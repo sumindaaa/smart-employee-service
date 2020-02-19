5b1837b69afa46c6fdba763a06d88dde84cdda9f
sumindaaa <sumi.amarasinghe@gmail.com>
2020-02-19 21:05:36 +1100

    Still I didn't do the service methods. will do and commit soon

--- src/main/java/com/sumindaa/demo/employeeservice/model/Employee.java
+++ src/main/java/com/sumindaa/demo/employeeservice/model/Employee.java
@@ -3,9 +3,49 @@ package com.sumindaa.demo.employeeservice.model;
 import java.util.Date;
 
 public class Employee {
-int id;
-char firstName;
-char lastName;
-Date dob;
-char gender;
+    private int id;
+    private char firstName;
+    private char lastName;
+    private Date dob;
+    private char gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(char firstName) {
+        this.firstName = firstName;
+    }
+
+    public char getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(char lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 }
