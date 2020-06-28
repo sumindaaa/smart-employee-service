9c22b23582be2260f65019114fb9ba8bcc6d3b6f
sumindaaa <sumi.amarasinghe@gmail.com>
2020-06-28 21:12:33 +1000

    removed all

--- src/main/java/com/sumindaa/demo/employeeservice/model/EmpRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sumindaa.demo.employeeservice.model;
-
-import com.sumindaa.demo.employeeservice.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpRepository extends CrudRepository<Employee, Integer> {
-}
