ac6987150e9981f493d7b5e28430e16d996b3ba3
sumindaaa <sumi.amarasinghe@gmail.com>
2020-06-29 20:17:50 +1000

    added files from initializer

---
+++ src/main/java/com/sumi/smartemployeeservice/SmartEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sumi.smartemployeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumi/smartemployeeservice/SmartEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumi.smartemployeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
